27c5ad4ef30f8af0429abebc6dba07ffc183645e
SGUENTEN <Siegfried-Dominic.Guentensperger@ejgallo.com>
2024-06-17 13:58:27 +0200

    Beginning of portfolio add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SkillsComponent} from "./skills/skills.component";
+import {HeaderComponent} from "./header/header.component";
+import {ProjectsComponent} from "./projects/projects.component";
+import {ExperienceComponent} from "./experience/experience.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SkillsComponent, HeaderComponent, ProjectsComponent, ExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
